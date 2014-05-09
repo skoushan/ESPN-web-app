402725429420a4a6b022de77e3b6109bd3112cce
Soheil Koushan <soheil.koushan@gmail.com>
2014-05-09 04:36:36 -0400

    added tests and new layout

--- js/main.js
+++ js/main.js
@@ -1,10 +1,5 @@
 // reference: https://software.intel.com/en-us/html5/articles/how-to-access-JSON-data-in-HTML5-apps
 // https://software.intel.com/en-us/html5/articles/how-to-access-JSON-data-in-HTML5-apps
-
-// function getArticles(json_url, data, callback){
-//     $.getJSON(json_url,)
-// }
-
 // callback introduction: http://callbackhell.com/
 
 var API_BASE = "http://api.espn.com/v1/";
@@ -12,81 +7,97 @@ var API_PATH = "sports/news";
 var API_NAME = "apikey";
 var API_KEY = "72hur4sdsjdbghuynm3z2t2k";
 var FULL_URL = API_BASE + API_PATH + "?" + API_NAME + "=" + API_KEY;
+// var FULL_URL = "http://skoushan.com/articles.json";
+var ERROR_MSG = "Unable to retrieve articles :(";
 
 function getArticles(JSON_url, callback) {
     $.getJSON(JSON_url)
         .done(function(json){
-            var articles = json['headlines'];
-            alert("done");
-            callback(articles);
+            var retrievedArticles = json['headlines'];
+            $('#retrieve-error').remove();
+            callback(retrievedArticles);
         })
         .fail(function(jqxhr, textStatus, error){
             var err = textStatus + ", " + error;
             console.log("Request Failed: " + error);
-            // $('#article-feed').text("<p style='color:red'>Unable to retrieve articles :(</p>");
-            alert("fail");
+            if($('#retrieve-error').length < 1)
+                $('body').prepend("<p id='retrieve-error'>" + ERROR_MSG + "</p>");
             callback(null);
     });
 }
 
-function determineChangedArticles() {
+function determineNewArticles(curArticles, retrievedArticles) {
+    var newArticles = [];
 
+    if(curArticles == null){
+        newArticles = retrievedArticles;
+    }
+    else if(retrievedArticles != null){
+        $.each(retrievedArticles, function(index, retrievedArticle){
+            var found = false;
+            $.each(curArticles, function(index, curArticle){
+                if(retrievedArticle.id == curArticle.id){
+                    found = true;
+                    return false;
+                }
+            });
+            if(!found){
+                newArticles.push(retrievedArticle);
+                console.log(retrievedArticle);
+            }
+        });
+    }
+
+    return newArticles;
 }
 
-
-function printArticles(oldArticles){
-    $.getJSON('http://api.espn.com/v1/sports/news?apikey=72hur4sdsjdbghuynm3z2t2k', function(data) {
-        var newArticles = data['headlines'];
-        
-        //http://stackoverflow.com/questions/6857468/a-better-way-to-convert-js-object-to-array
-        var newArticlesArray = $.map(newArticles, function(value, index){
-            return [value];
-        });
-
-        // http://stackoverflow.com/questions/17684921/sort-json-object-in-javascript
-        newArticlesArray.sort(function(a,b){
-            return new Date(b.published) - new Date(a.published);
-        });
-
-        var startingIndex = newArticlesArray.length-1;
-        if(oldArticles != null){
-            var indexNum =-1;
-            for(var i = 0; i < newArticlesArray.length; i++){
-                if(JSON.stringify(newArticlesArray[i]) === JSON.stringify(oldArticles[0])){
-                    indexNum = i;
-                    break;
-                }
-            }
-        }
-
-        if(oldArticles != null && indexNum != -1){
-            startingIndex = indexNum - 1;
-        }
-        
-        for(var i = startingIndex; i >=0; i--){
-            var img = "http://a.espncdn.com/i/espn/espn_logos/espn_red.png";
-            if(newArticlesArray[i].images && newArticlesArray[i].images[0]){
-                img = newArticlesArray[i].images[0].url;
-            }
-            var articleID = "article-" + newArticlesArray[i].id;
-            $('#article-feed').prepend($('<tr>',{id:articleID}));
-            $("#" + articleID).append($('<td>',{class:"thumbnail",}));
-            $("#" + articleID).append($('<td>',{class:"description"}));
-
-            $('#' + articleID + ' td.thumbnail').css('background-image', 'url('+img+')')
-            $('#' + articleID + ' td.description').prepend($('<div>', {
-                text: newArticlesArray[i].headline
-            }));
-        }
-
-        oldArticles = newArticlesArray;
-        console.log("fetched news");
+function printArticles(articles){
+    var default_img = "http://a.espncdn.com/i/espn/espn_logos/espn_red.png";
     
-        setTimeout(function(){printArticles(oldArticles)}, 30000);
+    articles.sort(function(a,b){
+        return new Date(a.published) - new Date(b.published);
+    });
+
+    $.each(articles, function(index, article){
+        img = default_img;
+        if(article.images && article.images[0]){
+            img = article.images[0].url;
+        }
+        var articleID = "article-" + article.id;
+        $('#article-feed').prepend($('<div>',{id:articleID, class:'article'}));
+        $("#" + articleID).append($('<div>',{class:"thumbnail"}));
+        $("#" + articleID).append($('<div>',{class:"details"}));
+
+        $('#' + articleID + ' .thumbnail').css('background-image', 'url('+img+')');
+
+        var url = "";
+        if(article.links && article.links.mobile){
+            url = article.links.mobile.href;
+        }
+        $('#' + articleID + ' .details').append($('<a>', {text: article.headline, class:'headline', href:url}));
+        
+        var date = moment(article.published).fromNow();
+      
+        $('#' + articleID + ' .details').append($('<span>', {text: date, class:'date'}));
+        $('#' + articleID + ' .details').append($('<span>', {text: article.source, class:'source'}));
+
+        var description = article.description.substr(0,30);
+        description = description.substr(0,description.lastIndexOf(' ')) + "...";
+        $('#' + articleID + ' .details').append($('<span>', {text: description, class:'description'}));
     });
 }
 
 $(document).ready(function(){ 
-    getArticles("hh", alert);
-    printArticles(null);
+    curArticles = [];
+
+    run = function(){
+        getArticles(FULL_URL, function(retrievedArticles){
+            var newArticles = determineNewArticles(curArticles, retrievedArticles);
+            printArticles(newArticles);
+            curArticles = curArticles.concat(newArticles);
+        });
+    }
+
+    run();
+    setInterval(run, 5000);
 });
--- spec/Spec.js
+++ spec/Spec.js
@@ -1,34 +1,68 @@
 //http://stackoverflow.com/questions/4662641/how-do-i-verify-jquery-ajax-events-with-jasmine
 
-describe("JSON", function() {
-    var main = {'getArticles':getArticles};
+//jasmine.getFixtures().fixturesPath = 'spec/fixtures';
+
+
+describe("getArticles with bad URL", function() {
     var callback;
     
     beforeEach(function(done) {
-        callback = {
-            'func': function(value) {
-                done();
-            }
-        }
-        spyOn(callback, 'func');
-        spyOn(main,'getArticles').and.callThrough();
-        main.getArticles("hh",callback.func);
+	   	setFixtures('<table id="article-feed"></table>');
+    	callback = jasmine.createSpy();
+		getArticles("bad url", function(articles) {
+			callback(articles);
+			done();
+		});
     });
-    it("should callback with null if url is invalid", function(){
-//        var callback = jasmine.createSpy();
-        expect(callback.func).toHaveBeenCalledWith(null);
+    it("should callback with null", function(){
+        expect(callback).toHaveBeenCalled();
     });
-////    var getArticles, callback;
-//    
-//    beforeEach(function(done) { 
-//        spyOn(main,'getArticles').and.callThrough();
-//        main.getArticles("hh",done);
-////        getArticles = jasmine.createSpy('getArticles').and.callThrough();
-////        getArticles("hh",done);
-//    });
-//        
-//    it("should have been called", function(done){
-//        expect(main.getArticles).toHaveBeenCalled();
-////        expect(getArticles).toHaveBeenCalled();
-//    });
+    it("should display error", function(){
+    	expect($('#retrieve-error').length).toEqual(1);
+    });
+});
+
+describe("getArticles with good URL", function(){
+    var articlesReturned;
+    
+    beforeEach(function(done) {
+		getArticles(FULL_URL, function(articles) {
+			articlesReturned = articles;
+			done();
+		});
+    });
+    it("should callback with non-null object", function(){
+        expect(articlesReturned).not.toBeNull();
+    });
+});
+
+var article0 = {'id':0, 'published':'2014-01-01T00:00:00Z'};
+var article1 = {'id':1, 'published':'2014-01-01T00:00:01Z'};
+var article2 = {'id':2, 'published':'2014-01-02T00:00:00Z'};
+var article3 = {'id':3, 'published':'2014-03-01T00:00:00Z'};
+var article4 = {'id':4, 'published':'2015-01-01T00:00:00Z'};
+
+var articles0 = [article0, article1];
+var articles1 = [article1, article2];
+
+describe("determineNewArticles", function(){
+	it("empty array when retrieved articles are null", function(){
+		expect(determineNewArticles(articles0, null).length).toEqual(0);
+	});
+
+	it("1 new article", function(){
+		expect(determineNewArticles(articles0, articles1)).toEqual([article2]);
+	});
+});
+
+describe("printArticles", function(){
+    beforeEach(function() {
+	   	setFixtures('<table id="article-feed"></table>');
+	});
+
+	it("should print in order", function(){
+		printArticles([article4, article2, article0, article1, article3]);
+		var article_feed = document.getElementById('article-feed');
+		expect(article_feed.length).toEqual(5);
+	});
 });
